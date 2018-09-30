fa10d9329e195a803092cd00f31334445fefe3db
Ripal Patel <ripal212007@gmail.com>
2018-09-30 11:36:20 -0500

    added more function for addition multiplication and subtrction

--- javascripts/helpers/buttonEvents.js
+++ javascripts/helpers/buttonEvents.js
@@ -1,21 +1,68 @@
 import{setMathType,addNumber, calculate} from '../components/calculator.js'
-
+const oneButton = document.getElementById('oneButton');
+const twoButton = document.getElementById('twoButton');
+const threeButton = document.getElementById('threeButton');
+const fourButton = document.getElementById('fourButton');
+const fiveButton = document.getElementById('fiveButton');
+const sixButton = document.getElementById('sixButton');
 const sevenButton = document.getElementById('sevenButton');
 const eightButton = document.getElementById('eightButton');
 const nineButton = document.getElementById('nineButton');
 const equalButton = document.getElementById('equalButton');
-const divideButton = document.getElementById('divideButton')
+const divideButton = document.getElementById('divideButton');
+const addButton = document.getElementById('addButton');
+const multiplyButton = document.getElementById('multiplyButton');
+const subtractButton =document.getElementById('subtractButton');
 
 const addNumberCaller = (e) => {
     addNumber(e.target.innerHTML);
 }
-
+// function to divide numbers
 const divideButtonEvent = () => {
     divideButton.addEventListener('click', (e)=>{
         e.target.className += 'selectedButton';
         setMathType('divide');
     })
 } 
+// function to multiply numbers
+const multiplyButtonEvent = () => {
+    multiplyButton.addEventListener('click', (e)=>{
+        e.target.className += 'selectedButton';
+        setMathType('multiply');
+    })
+}
+// function to add numbers
+const addButtonEvent = () => {
+    addButton.addEventListener('click', (e)=>{
+        e.target.className += 'selectedButton';
+        setMathType('add');
+    })
+}
+// function to subtract number
+const subtractButtonEvent = () => {
+    subtractButton.addEventListener('click', (e)=>{
+        e.target.className += 'selectedButton';
+        setMathType('subtract');
+    })
+}
+const oneButtonEvent = () => {
+    oneButton.addEventListener('click', addNumberCaller);
+}
+const twoButtonEvent = () => {
+    twoButton.addEventListener('click', addNumberCaller);
+}
+const threeButtonEvent = () => {
+    threeButton.addEventListener('click', addNumberCaller);
+}
+const fourButtonEvent = () => {
+    fourButton.addEventListener('click', addNumberCaller);    
+}
+const fiveButtonEvent = () => {
+    fiveButton.addEventListener('click', addNumberCaller);
+}
+const sixButtonEvent = () => {
+    sixButton.addEventListener('click', addNumberCaller);
+}
 const sevenButtonEvent = () => {
     sevenButton.addEventListener('click', addNumberCaller);
 };
@@ -35,10 +82,19 @@ const equalButtonEvent = () => {
 }
 
 const attachEvents = () => {
+    oneButtonEvent();
+    twoButtonEvent();
+    threeButtonEvent();
+    fourButtonEvent();
+    fiveButtonEvent();
+    sixButtonEvent();
     sevenButtonEvent();
     eightButtonEvent();
     nineButtonEvent();
     equalButtonEvent();
     divideButtonEvent();
+    multiplyButtonEvent();
+    addButtonEvent();
+    subtractButtonEvent();
 }
 export default attachEvents;
